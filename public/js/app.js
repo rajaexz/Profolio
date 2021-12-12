3f57f10cdb856ee44195a5cf47bfd8e2b1882544
unknown <deepkexz@gmail.com>
2021-12-12 18:22:28 +0530

    make it

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,11 @@
 //++++++++++++++++++++ Curser Efact  ++++++++++++++++++++++++++
 
-        const cursor = document.getElementById('cursor');
-        document.onmousemove = function (e) {
-            cursor.style.left = (e.pageX - 25) + "px";
-            cursor.style.top = (e.pageY - 25) + "px";
-            cursor.style.display = "block";
-        }
+        // const cursor = document.getElementById('cursor');
+        // document.onmousemove = function (e) {
+        //     cursor.style.left = (e.pageX - 25) + "px";
+        //     cursor.style.top = (e.pageY - 25) + "px";
+        //     cursor.style.display = "block";
+        // }
 
 
 // ++++++++++++++++++++++Owl Carousel++++++++++++++++++++++++++++
@@ -25,7 +25,7 @@ var valuelements = document.getElementsByClassName("count");
 var features = new Array(valuelements.length);
 
  for (var j=0; j<=features.length; j++){ 
-  var val = valuelements[j].innerHTML;
+  var val = valuelements[j];
   const total = (val/100);
   for (i = 0; i < svgCalss.length; ++i) {
 
